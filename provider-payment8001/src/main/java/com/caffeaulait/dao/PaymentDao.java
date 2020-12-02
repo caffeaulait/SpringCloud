3b8df1f5e38e89f24098ac08e3ec1a687e671321
Yang Lu <yang.youngsoul@gmail.com>
2020-12-02 23:21:11 +0800

    first commit

---
+++ provider-payment8001/src/main/java/com/caffeaulait/dao/PaymentDao.java
@@ -0,0 +1,12 @@
+package com.caffeaulait.dao;
+
+import com.caffeaulait.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface PaymentDao {
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long id);
+}
---
+++ provider-payment8002/src/main/java/com/caffeaulait/service/PaymentServiceImpl.java
@@ -0,0 +1,22 @@
+package com.caffeaulait.service;
+
+import com.caffeaulait.dao.PaymentDao;
+import com.caffeaulait.entities.Payment;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService{
+
+    @Resource
+    private PaymentDao paymentDao;
+
+    public int create(Payment payment){
+        return paymentDao.create(payment);
+    }
+
+    public Payment getPaymentById(Long id){
+        return paymentDao.getPaymentById(id);
+    }
+}
